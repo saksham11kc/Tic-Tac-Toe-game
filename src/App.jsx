b5fc54983533537adec05d845c95aa0c75e72a63
Saksham KC <sakshamkc@Sakshams-MacBook-Air.local>
2024-09-05 14:40:20 +0545

    Make container for game

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+        </ol>
+      </div>
+    </main>
+  );
+}
+
+export default App;
